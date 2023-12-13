2799f296a754dbeff86bbbd32f17f3fa4c5c1300
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-13 14:40:56 -0500

    wrote tests, wrote the replace methods

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -34,6 +36,26 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public String hamletToLeon(String hamletText){
+        String hamlet = "Hamlet";
+        Pattern pattern = Pattern.compile(hamlet);
+        Matcher matcher = pattern.matcher(hamletText);
+
+        String hamletToLeon = matcher.replaceAll("Leon");
+
+        return hamletToLeon;
+    }
+
+    public String horatioToTariq(String hamletText){
+        String horatio = "Horatio";
+        Pattern pattern = Pattern.compile(horatio);
+        Matcher matcher = pattern.matcher(hamletText);
+
+        String horatioToTariq = matcher.replaceAll("Tariq");
+
+        return horatioToTariq;
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,41 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String ham = "Hamlet";
+        String expected = "Leon";
+        HamletParser hp = new HamletParser();
+
+        String actual = hp.hamletToLeon(ham);
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String hor = "Horatio";
+        String expected = "Tariq";
+        HamletParser hp = new HamletParser();
+
+        String actual = hp.horatioToTariq(hor);
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        // maybe instead of boolean, this method should return
+        // what index the first instance of horatio is at
+        String testStr = "This string has Horatio in it.";
+        HamletParser hp = new HamletParser();
+
+        Assert.assertTrue(hp.findHoratio(testStr));
     }
 
     @Test
     public void testFindHamlet() {
+        String testStr = "This string has Hamlet in it";
+        HamletParser hp = new HamletParser();
+
+        Assert.assertTrue(hp.findHamlet(testStr));
     }
 }
